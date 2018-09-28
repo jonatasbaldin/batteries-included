dee3ce14ebae24d7063f6adaeb1ea86f6816d8bf
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-09-28 12:23:25 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🔋✨ Batteries Included ✨🔋
+A talk about the Python Standard Library.
+
+### Run
+Requries Python 3.7.
+```
+$ pipenv install
+$ pipenv run jupyter notebook
+```
+
+### License
+MIT.
---
+++ mult.py
@@ -0,0 +1,17 @@
+# defines the mult method
+
+def mult(a, b):
+    """
+    Multiplies a and b.
+
+    >>> a, b = 2, 5
+    >>> mult(a, b)
+    10
+
+    # Using traceback
+    >>> mult(1, 2, 3)
+    Traceback (most recent call last):
+    TypeError: mult() takes 2 positional arguments but 3 were given
+    """
+
+    return a * b
