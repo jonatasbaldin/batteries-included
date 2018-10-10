cc5dba8c72429505cd60f519469e7d5b24ee5b96
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-10-10 13:00:23 +0200

    MOAR changes

---
+++ multiply.py
@@ -0,0 +1,14 @@
+def multiply(a, b):
+    """
+    Multiplies a and b.
+
+    >>> a, b = 2, 5
+    >>> multiply(a, b)
+    10
+
+    >>> multiply(1, 2, 3)
+    Traceback (most recent call last):
+    TypeError: multiply() takes 2 positional arguments but 3 were given
+    """
+
+    return a * b
